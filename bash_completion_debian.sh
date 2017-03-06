1a839f17c9a8d2026f513dd78c6d665764434f97
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-03-06 20:03:47 -0300

    Fix bash completion

--- bash_completion_debian.sh
+++ bash_completion_debian.sh
@@ -7,11 +7,14 @@ _usdocker()
   cur="${COMP_WORDS[COMP_CWORD]}"
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
-  if [ "$prev" != "usdocker" ]
+  if [ "$prev" == "usdocker" ]
+  then
+      _script_commands=$(usdocker .autocomplete get)
+  elif [ "$COMP_CWORD" == "2" ]
   then
       _script_commands=`usdocker .autocomplete get $prev`
   else
-      _script_commands=$(usdocker .autocomplete get)
+      _script_commands=""
   fi
 
   COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
