5ea4a18a432390630f1ff39f4e3d4a0124a52ddd
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-03-08 23:42:20 -0300

    Create install script

---
+++ install/.createinstall.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+if [ -x "`which realpath`" ]
+then
+    BASEDIR=`realpath $0`
+    BASEDIR=`dirname $BASEDIR`
+else
+    BASEDIR="$(dirname "$(readlink -f "$0")")"
+fi
+
+cd $BASEDIR/../../
+echo "From: $PWD"
+
+if [ -z "$1" ]; then
+    echo Please specify the script name
+    exit 1
+fi
+
+PAYLOAD=__usdocker__$RANDOM.tar.gz
+SCRIPT=$1
+TMP=__extract__$RANDOM
+
+tar czvf $PAYLOAD usdocker
+
+printf "#!/usr/bin/env bash
+if [[ \$EUID -ne 0 ]]; then
+   echo \"This script must be run as root\" 1>&2
+   exit 1
+fi
+rm -rf /opt/usdocker
+PAYLOAD_LINE=\`awk '/^__PAYLOAD_BELOW__/ {print NR + 1; exit 0; }' \$0\`
+tail -n+\$PAYLOAD_LINE \$0 | tar -xvz -C /opt
+/opt/usdocker/install/install.sh
+exit 0
+__PAYLOAD_BELOW__\n" > "$TMP"
+
+cat "$TMP" "$PAYLOAD" > "$SCRIPT"
+chmod +x "$SCRIPT"
+rm "$TMP"
+rm "$PAYLOAD"
---
+++ install/install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+if [ -x "`which realpath`" ]
+then
+    BASEDIR=`realpath $0`
+    BASEDIR=`dirname $BASEDIR`
+else
+    BASEDIR="$(dirname "$(readlink -f "$0")")"
+fi
+
+echo "Creating symbolic link..."
+ln -sf "`dirname $BASEDIR`/usdocker.sh" /usr/local/bin/usdocker
+
+eval $(cat /etc/*-release)
+
+if [ "$ID_LIKE" == "debian" ]; then
+    echo "Setting auto-complete..."
+    ln -sf "`dirname $BASEDIR`/bash_completion_debian.sh" /etc/bash_completion.d/usdocker
+fi
