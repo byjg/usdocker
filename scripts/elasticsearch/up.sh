ea37a75fed29e03cf7dca7585a470b1a6d4735b8
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-03-20 11:17:27 -0300

    Minor change in elasticsearch

--- scripts/elasticsearch/up.sh
+++ scripts/elasticsearch/up.sh
@@ -7,14 +7,10 @@ source "$USD_SCRIPTS/elasticsearch/setup.sh"
 docker run \
     --name elasticsearch${CONTAINER_NAME_SUFFIX} \
     --cap-add=IPC_LOCK \
-    --ulimit \
-    memlock=-1:-1 \
+    --ulimit memlock=-1:-1 \
     --ulimit nofile=65536:65536 \
-    -e "bootstrap.memory_lock=true" \
     -e ES_JAVA_OPTS="-Xms${ELASTICSEARCH_MEMORY} -Xmx${ELASTICSEARCH_MEMORY}" \
     --publish-all \
-    -e "http.host=0.0.0.0" \
-    -e "transport.host=127.0.0.1" \
     -p "${ELASTICSEARCH_PORT}:9200" \
     -v `adjustLocalDirectories "${ELASTICSEARCH_FOLDER}/data" "/usr/share/elasticsearch/data"` \
     -v `adjustLocalDirectories "$USD_HOME/elasticsearch/conf/custom.yml" /usr/share/elasticsearch/config/elasticsearch.yml` \
