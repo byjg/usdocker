bf1418e9fb81cfec4212fcd0d4a76262bbf494ed
Joao Gilberto Magalhães <joao@byjg.com.br>
2017-06-16 19:18:28 -0300

    Added a message after install docker.

--- scripts/docker/install.sh
+++ scripts/docker/install.sh
@@ -10,3 +10,9 @@ wget -qO- https://get.docker.com/ | sh
 groupadd docker
 gpasswd -a $(who -m | awk '{print $1;}') docker
 service docker restart
+
+echo "-------------------------------------------------------------------------------------"
+echo "Docker add to the user '$(who -m | awk '{print $1;}')'"
+echo "You need to run 'newgrp docker' or logout in order to get docker working to this user'
+echo "-------------------------------------------------------------------------------------"
+echo 
