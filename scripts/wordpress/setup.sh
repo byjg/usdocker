18127f61ac29bae05dca779901139e9080893f99
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-06-21 19:54:13 -0300

    Update Wordpress setup.sh

--- scripts/wordpress/setup.sh
+++ scripts/wordpress/setup.sh
@@ -2,10 +2,11 @@
 
 # Environment
 setupEnvironment ${USD_SERVICE}/environment IMAGE wordpress:4.8-php7.1
-setupEnvironment ${USD_SERVICE}/environment PLUGIN_FOLDER \${USD_DATA}/\${USD_SERVICE}/plugins
-setupEnvironment ${USD_SERVICE}/environment THEME_FOLDER \${USD_DATA}/\${USD_SERVICE}/themes
-setupEnvironment ${USD_SERVICE}/environment UPLOAD_FOLDER \${USD_DATA}/\${USD_SERVICE}/uploads
-setupEnvironment ${USD_SERVICE}/environment LANGUAGES_FOLDER \${USD_DATA}/\${USD_SERVICE}/languages
+setupEnvironment ${USD_SERVICE}/environment DATAFOLDER \${USD_DATA}/\${USD_SERVICE}
+setupEnvironment ${USD_SERVICE}/environment PLUGIN_FOLDER \${WORDPRESS_DATAFOLDER}/plugins
+setupEnvironment ${USD_SERVICE}/environment THEME_FOLDER \${WORDPRESS_DATAFOLDER}/themes
+setupEnvironment ${USD_SERVICE}/environment UPLOAD_FOLDER \${WORDPRESS_DATAFOLDER}/uploads
+setupEnvironment ${USD_SERVICE}/environment LANGUAGES_FOLDER \${WORDPRESS_DATAFOLDER}/languages
 setupEnvironment ${USD_SERVICE}/environment PORT 8080
 setupEnvironment ${USD_SERVICE}/environment DB_HOST mysql\${CONTAINER_NAME_SUFFIX}:3306
 setupEnvironment ${USD_SERVICE}/environment DB_USER root
@@ -31,21 +32,38 @@ then
     cp -r "$USD_SCRIPTS/${USD_SERVICE}/conf" "$USD_HOME/${USD_SERVICE}"
 fi
 
+if [ ! -d "$WORDPRESS_DATAFOLDER" ]
+then
+    sudo mkdir -p "$WORDPRESS_DATAFOLDER"
+    sudo chown 33:33 -R "$WORDPRESS_DATAFOLDER"
+fi
+
+if [ ! -d "$WORDPRESS_PLUGIN_FOLDER" ]
+then
+    sudo mkdir -p "$WORDPRESS_PLUGIN_FOLDER"
+    sudo chown 33:33 -R "$WORDPRESS_PLUGIN_FOLDER"
+fi
+
+if [ ! -d "$WORDPRESS_THEME_FOLDER" ]
+then
+    sudo mkdir -p "$WORDPRESS_THEME_FOLDER"
+    sudo chown 33:33 -R "$WORDPRESS_THEME_FOLDER"
+fi
+
 if [ ! -d "$WORDPRESS_UPLOAD_FOLDER" ]
 then
-    mkdir -p "$WORDPRESS_UPLOAD_FOLDER"
-    echo
-    echo "# It is necessary adjust permission as superuser."
-    echo "# If the command below fails, please execute it manually before start using"
-    echo
-    echo "# ----------------------------------------------"
-    echo "# sudo chown 33:33 \"$WORDPRESS_UPLOAD_FOLDER\""
-    echo "# ----------------------------------------------"
-    echo
-    sudo chown 33:33 "$WORDPRESS_UPLOAD_FOLDER"
+    sudo mkdir -p "$WORDPRESS_UPLOAD_FOLDER"
+    sudo chown 33:33 -R "$WORDPRESS_UPLOAD_FOLDER"
 fi
 
 if [ ! -d "$WORDPRESS_LANGUAGES_FOLDER" ]
 then
-    mkdir -p "$WORDPRESS_LANGUAGES_FOLDER"
+    sudo mkdir -p "$WORDPRESS_LANGUAGES_FOLDER"
+    sudo chown 33:33 -R "$WORDPRESS_LANGUAGES_FOLDER"
+fi
+
+if [ ! -d "$WORDPRESS_PLUGIN_FOLDER" ] || [ ! -d "$WORDPRESS_PLUGIN_FOLDER" ] || [ ! -d "$WORDPRESS_PLUGIN_FOLDER" ] || [ ! -d "$WORDPRESS_PLUGIN_FOLDER" ]; then
+    echo
+    echo "We cannot create the WORDPRESS DATA FOLDER. Please enable SUDO and continue.".
+    exit
 fi
