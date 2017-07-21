22f05c8624b8443664760936cb32163ba55501f5
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-07-21 14:31:38 -0300

    Added more setup options for Elasticsearch

--- scripts/elasticsearch/setup.sh
+++ scripts/elasticsearch/setup.sh
@@ -2,9 +2,17 @@
 
 # Setup Environment
 setupEnvironment ${USD_SERVICE}/environment IMAGE docker.elastic.co/elasticsearch/elasticsearch:5.2.2
-setupEnvironment ${USD_SERVICE}/environment FOLDER \${USD_DATA}/\${USD_SERVICE}
+setupEnvironment ${USD_SERVICE}/environment FOLDER \${USD_DATA}/${USD_SERVICE}
 setupEnvironment ${USD_SERVICE}/environment MEMORY 1g
 setupEnvironment ${USD_SERVICE}/environment PORT 9200
+setupEnvironment ${USD_SERVICE}/environment TRANSPORT 9300
+setupEnvironment ${USD_SERVICE}/environment CLUSTER_NAME docker-cluster
+setupEnvironment ${USD_SERVICE}/environment NODENAME $(hostname)
+setupEnvironment ${USD_SERVICE}/environment PUBLISH_ADDRESS $(ip route get 1 | head -1 | cut -d' ' -f8)
+setupEnvironment ${USD_SERVICE}/environment DISCOVERY_NODES
+setupEnvironment ${USD_SERVICE}/environment NODE_MASTER true
+setupEnvironment ${USD_SERVICE}/environment NODE_DATA true
+setupEnvironment ${USD_SERVICE}/environment NODE_INGEST true
 
 source "$USD_HOME/${USD_SERVICE}/environment"
 
@@ -25,6 +33,7 @@ fi
 
 if [ ! -d "$USD_DATA/${USD_SERVICE}/data" ]
 then
+    echo "Fixing directories permission (requires sudo)"
     mkdir -p "$USD_DATA/${USD_SERVICE}/data"
     sudo chown 1000:1000 -R "$USD_DATA/${USD_SERVICE}/data"
 fi
--- scripts/elasticsearch/up.sh
+++ scripts/elasticsearch/up.sh
@@ -4,18 +4,31 @@ dockerMachineWarning
 
 source "$USD_SCRIPTS/${USD_SERVICE}/setup.sh"
 
+if [ ! -z "ELASTICSEARCH_DISCOVERY_NODES" ]; then
+    DISCOVERY=" -e discovery.zen.ping.unicast.hosts=$ELASTICSEARCH_DISCOVERY_NODES "
+fi
+
+echo "fixing vm.max_map_count (requires sudo)"
+sudo sysctl -w vm.max_map_count=262144
+
 docker run \
     --name ${USD_SERVICE}${CONTAINER_NAME_SUFFIX} \
     --cap-add=IPC_LOCK \
     --ulimit memlock=-1:-1 \
     --ulimit nofile=65536:65536 \
+    -e TZ=${TZ} \
     -e ES_JAVA_OPTS="-Xms${ELASTICSEARCH_MEMORY} -Xmx${ELASTICSEARCH_MEMORY}" \
-    --publish-all \
+    -e bootstrap.memory_lock=true \
+    -e cluster.name=$ELASTICSEARCH_CLUSTER_NAME \
+    -e node.name=$ELASTICSEARCH_NODE_NAME \
+    -e network.publish_host=$ELASTICSEARCH_PUBLISH_ADDRESS \
+    -e node.master=$ELASTICSEARCH_NODE_MASTER \
+    -e node.data=$ELASTICSEARCH_NODE_DATA \
+    -e node.ingest=$ELASTICSEARCH_NODE_INGEST \
     -p "${ELASTICSEARCH_PORT}:9200" \
+    -p "${ELASTICSEARCH_TRANSPORT}:9300" $DISCOVERY \
     -v `adjustLocalDirectories "${ELASTICSEARCH_FOLDER}/data" "/usr/share/elasticsearch/data"` \
     -v `adjustLocalDirectories "$USD_HOME/${USD_SERVICE}/conf/custom.yml" /usr/share/elasticsearch/config/elasticsearch.yml` \
     -d ${ELASTICSEARCH_IMAGE}
 
 checkIsRunning ${USD_SERVICE}${CONTAINER_NAME_SUFFIX}
-
-#    -e TZ=${TZ} \
